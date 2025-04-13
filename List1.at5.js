d4c729c34d254377b2b9d7ff1a58467b6de289dc
Giovanna França <francamariagiovanna4@gmail.com>
2025-04-13 16:56:19 -0300

    Exercicio Detetivo de Dados em JavaScript

---
+++ List1.at5.js
@@ -0,0 +1,10 @@
+let suspeito1 = "42"
+let suspeito2 = 42
+
+if (suspeito1 === suspeito2){
+    console.log ("Eles dizem a mesma coisa, mas de formas diferentes.")
+} else if (suspeito1 == suspeito2){
+    console.log ("Eles são exatamente iguais.")
+}else {
+    console.log ("Eles são diferentes.")
+}
