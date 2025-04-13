67c3fefa9a4a2fae876db4fb8e54bdd2a45f1bd5
Giovanna França <francamariagiovanna4@gmail.com>
2025-04-13 17:10:28 -0300

    Exercicio do Robô  do Café em JavaScript

---
+++ List1.at7.js
@@ -0,0 +1,12 @@
+let pedido
+
+if (pedido == café){
+    console.log ("Servindo cafe")
+}else if (pedido == capuccino){
+    console.log ("Servindo capuccino")
+}else if (pedido == expresso){
+    console.log ("Servindo seu expresso")
+}else{
+    console.log ("Desculpe não temos esse tipo de café")
+    
+}
