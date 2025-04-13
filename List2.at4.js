789bc5ba62c6c86f8158448865bfba323d9e3258
Giovanna França <francamariagiovanna4@gmail.com>
2025-04-13 17:03:28 -0300

    Exercicio Suco de Frutas em JavaScript

---
+++ List2.at4.js
@@ -0,0 +1,11 @@
+let fruta = prompt("Digite o nome da fruta")
+
+if (fruta === "laranja"){
+    console.log ("Preparando seu suco...")
+} else if (fruta === "uva"){
+    console.log("Preparando seu suco...")
+} else if (fruta === "morango"){
+    console.log ("Preparando seu suco...")
+}else{
+    console.log ("Essa fruta não está disponível :(")
+}
