5ec4668fdc4801fb1a2363dfddf79c563bd51e35
francagiovanna <francamariagiovanna4@gmail.com>
2025-04-17 21:00:37 -0300

    Adicionei calculadora de  botão

---
+++ exercios-sala01/exercicio.js
@@ -0,0 +1,20 @@
+function somar(){
+    let n1, n2, soma
+
+    n1 = Number (prompt("Primeiro número: "))
+    n2 = Number (prompt("Segundo número: "))
+    
+    soma= n1 + n2
+
+    alert ("Soma: " + soma)
+}
+function diminuir(){
+    let n1,  n2, diferenca
+
+    n1 = Number (prompt("Primeiro numero"))
+    n2 = Number (prompt("Segundo numero"))
+    
+    diferenca = n1 - n2
+
+    alert("Diferença: " + diferenca)
+}
