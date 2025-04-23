006e4ef15fbdeffab6bb663b5421b40608f78100
francagiovanna <francamariagiovanna4@gmail.com>
2025-04-22 21:56:45 -0300

    Atualizações de conhecimento em sala

---
+++ exercios-sala01/att1.js
@@ -0,0 +1,17 @@
+// Teste de puxar botão
+function mostraroi(){
+alert("Oi mundo")
+}
+function exercicio1(){
+    let num1 = Number(prompt("Digite um número"))
+    let num2 = Number(prompt("Digite outro número"))
+    let num3 = Number(prompt("Digite outro número"))
+    let soma = num1 + num2 + num3
+    alert(" A soma dos dois números é:" + soma   )
+}
+function exercicio2(){
+    let num1 = Number(prompt("Digite um número"))
+    let num2 =  Number(prompt("Digite outro número"))
+    let media = (num1 + num2) / 2
+    alert ("A media dos dois números é: " + media)
+}
