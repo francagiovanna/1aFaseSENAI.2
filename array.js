6beee790f902e538eede587e2d67d63fa4a26524
francagiovanna <francamariagiovanna4@gmail.com>
2025-06-10 21:59:20 -0300

    Tela crud

---
+++ array.js
@@ -0,0 +1,30 @@
+// let nomes = ["joão" , "Clara"]
+// // console.log(nomes[2])
+// // console.log(nomes.length)
+
+// for(let contador = 0 ; contador < nomes.length ; contador++){
+//     console.log(nomes[contador])
+// }
+// nomes[2] = "Alex"
+// console.log(nomes)
+
+// numweoa = [10, 20, 30, 40,50, 60, 70, 80, 90, 100]
+
+// numerosDois = numeros.map(elemento => elemento*2)
+// console.log(numeros)
+
+// console.log(numerosDois)
+
+// let soma = numeros.reduce((acumulado, valoratual) => acumulado+valoratual, 0)
+// console.log(soma)
+
+// let nomes2 = ["João", "Clara", "Jonas", "Carla", "Maria", "Alessandro", "Carlos", "José"]
+
+// let nomesvirgula = 
+// nomes2.reduce((acumulado, valoratual) => acumulado + "," + valoratual, "")
+
+// console.log(nomesvirgula)
+
+let numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+let numerosinvertido = 
