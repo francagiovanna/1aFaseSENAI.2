67716ffb8c90e06d2782293b5d390171a2748c5d
francagiovanna <francamariagiovanna4@gmail.com>
2025-06-23 21:36:07 -0300

    Upgrade Objetos

---
+++ objetos.js
@@ -0,0 +1,9 @@
+//ultilizando new object
+let obj = new Object()
+
+//prpriedades e metodos
+let aluno = {nome: "Alex", apresentar: function(){return 'olá meu nome é: ${this.nome}'}}
+
+//acessando e modificando
+let objeto = {nome:'Gol', "numero-chassi": 2345334}
+objeto.nome = "editei"
